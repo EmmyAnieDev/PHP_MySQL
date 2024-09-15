08c8067308ff3e755c2ad568ab3c3461491efb7b
EmmyAnieDev <Emaduilzjr1@gmail.com>
2024-09-15 16:05:57 +0100

    require and include

--- index.php
+++ index.php
@@ -1,43 +1,15 @@
 <?php
 
-//  //////       VRIABLE SCOPE
-// /// // GLOBAL SCOPE
+//  //////       REQUIRE && INQUIRE
 
-$name = 'Mario';       // variable created outside a function or in the file can be accesed globally.
-
-function personName(){   
-
-    global $name;                // use the keyword global.
-    // $name = 'Yoshi';            // overide the global variable value
-    echo "Hello $name <br />";
-
-}
-
-personName();
-echo $name . '<br \>';              // if the global variable value has be overidden, this will echo out the overidden data.
+// // include another file into this file. if file does not exists it skips and run continue running the scripts
+include('ninjas.php');     // used for optional files
 
 
-function sayBye($name){              // local params
-    $name ='Jyu';          
-    echo "bye $name <br />";
-}
-
-sayBye($name);                      // passing the global variable as the argument of the function.
-
-
-
-//////            LOCAL SCOPE
-
-function personAge(){
-
-    $age = 26;     // variable created inside the function can only be used inside the function making it a local variable
-    echo "you're $age years old <br />";
-
-}
-
-personAge();
-
-
+// // include another file into this file. if file does not exists it returns a fatal error and the script stops execution.
+require 'ninjas.php';     // used for manadatory files
+ 
+echo 'end of php';
 
 
 ?>
@@ -47,7 +19,10 @@ personAge();
         <title> Test </title>
     </head>
     <body>
-  
+
+    <?php  include 'content.php' ?>
+    <?php  include 'content.php' ?>
+    <?php  include 'content.php' ?>
     
     </body>
 </html>
---
+++ ninjas.php
@@ -0,0 +1,6 @@
+<?php
+
+    $ninjas = ['Shaun', 'Ryu', 'Yoshi'];
+    echo "{$ninjas[1]}  <br />";
+
+?>
