6d7a20e985220feda43dba2956ede96f79ace177
EmmyAnieDev <Emaduilzjr1@gmail.com>
2024-09-18 22:42:49 +0100

    rendering data to browser

--- index.php
+++ index.php
@@ -22,12 +22,12 @@
     }
 
     // write query to select particular column from pizzas's table
-    $sql = 'SELECT title, ingredients, id FROM pizzas';
+    $sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
 
     // make query & get result
     $result = mysqli_query($conn, $sql);
 
-    // fetch the resulting rows as an associate array
+    // fetch the resulting rows as a multi-dimensional associate array
     $pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
     // free results from memory
@@ -36,8 +36,7 @@
     // end connection
     mysqli_close($conn);
 
-    print_r($pizzas);
-
+    // print_r($pizzas);
 ?>
 
 
@@ -45,6 +44,30 @@
 <!DOCTYPE html>
 <html>
 <?php include "templates/header.php"; ?>
+
+<h4 class="center grey-text">Pizzas!</h4>
+
+<div class="container">
+    <div class="row">
+
+        <?php foreach($pizzas as $pizza){ ?>
+
+            <div class="col s6 md3">
+                <div class="card z-depth-0">
+                    <div class="card-content center">
+                        <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+                        <div><?php echo htmlspecialchars($pizza['ingredients']); ?></div>
+                    </div>
+                    <div class="card-action right-align">
+                        <a href="#" class="brand-text">more info</a>
+                    </div>
+                </div>
+            </div>
+
+        <?php } ?>
+
+    </div>
+</div>
 
 <?php include "templates/footer.php"; ?>
         
