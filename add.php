b984d75f5172511e92c794e979e9043ac8b26cbd
EmmyAnieDev <Emaduilzjr1@gmail.com>
2024-09-21 12:05:47 +0100

    super globals

--- add.php
+++ add.php
@@ -77,7 +77,7 @@ if(isset($_POST['submit'])){
 
 <section class="container grey-text">
     <h4 class="center">Add a Pizza</h4>
-    <form class="white" action="add.php" method="POST">
+    <form class="white" action="<?php  echo $_SERVER['PHP_SELF']  ?>" method="POST">
         <label>Your Email:</label>
         <input type="text" name="email" value="<?php echo htmlspecialchars($email) ?>">
         <div class="red-text"><?php echo $errors['email']; ?></div>
---
+++ super_globals.php
@@ -0,0 +1,30 @@
+<?php
+
+ // Super globals
+
+ // $_GET['name'], $_POST['name']
+
+ echo $_SERVER['SERVER_NAME'] . '<br />';
+ echo $_SERVER['REQUEST_METHOD'] . '<br />';
+ echo $_SERVER['SCRIPT_FILENAME'] . '<br />';
+ echo $_SERVER['PHP_SELF'] . '<br />';
+ 
+
+
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title>Php tuts</title>
+        </head>
+        <body>
+            
+        </body>
+    
+</html>
