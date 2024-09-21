9167e7702ac604499530aa9f23aba3303aaacd17
EmmyAnieDev <Emaduilzjr1@gmail.com>
2024-09-21 11:44:06 +0100

    ternary operator

---
+++ ternary_operator.php
@@ -0,0 +1,28 @@
+<?php
+
+ // ternary operator
+
+ $score = 100;
+
+ $val = $score < 100 ? 'true' : 'false'; 
+
+ echo $val;
+
+
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title>Php tuts</title>
+        </head>
+        <body>
+            
+        </body>
+    
+</html>
