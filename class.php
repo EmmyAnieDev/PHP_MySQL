6c806d1cd7262b063954ce0dae10e663072e50d5
EmmyAnieDev <Emaduilzjr1@gmail.com>
2024-09-23 17:35:26 +0100

    final commit

--- class.php
+++ class.php
@@ -7,25 +7,40 @@ error_reporting(E_ALL);
 
 // // CLASS AND OBJECT
 // create our own data type called User.
+class User {
 
-class User{
 
-    //public properties can be accessed and modify outside the class
+    // Private properties (only accessible within the class)
     private $name;
-    public $email;
- 
-    // constructor 
-    public function __construct($email, $name){
-        $this->email = $email;
-        $this->name = $name;
+    private $email;
+
+    // Constructor to initialize properties when creating an object
+    public function __construct($email, $name) {
+        $this->email = $email; 
+        $this->name = $name;  
     }
 
-    // methods
-    function login(){
-        echo "current user is: $this->name and logged in as: $this->email <br />";
+    // Getter method to retrieve the user's name
+    public function getName() {
+        return $this->name;
+    }
+
+    // Setter method to update the user's name with validation
+    public function setName($name) {
+        if (is_string($name) && strlen($name) > 1) {
+            $this->name = $name; 
+            return "Name has been updated to $name";
+        } else {
+            return "Not a valid name";
+    }
+
+    // Method to simulate user login and display current user's information
+    public function login() {
+        echo "Current user is: $this->name and logged in as: $this->email <br />";
     }
 
 }
+
 
 // creating an object for the class
 $userOne = new User('mario@uk.co', 'Mario');
@@ -34,12 +49,10 @@ $userTwo = new User('yoshi@net.co', 'Yoshi');
 echo $userOne->login();
 echo $userTwo->login();
 
-// since properties are public, value can be modified.
-$userTwo->name = 'Ryu';
-$userTwo->email = 'Ryu@change.co';
+// since properties are private, use setName function to change name
+$userTwo->setName('Ryu');
 
 echo $userTwo->login();
-
 
 
 
